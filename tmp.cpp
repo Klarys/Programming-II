7d86058431ffbffb0bba65883ad3ba53325c7e26
Klarys <44509197+Klarys@users.noreply.github.com>
2019-01-12 09:47:16 +0100

    1
    1

---
+++ tmp.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <vector>
+#include <set>
+using namespace std;
+
+
+main()
+{
+    int cases;
+    cin>>cases;
+    while(cases>0)
+    {
+        int snowflakes_counter, tmp;
+        int result;
+        int current_max;
+
+        cin>>snowflakes_counter;
+        tmp=snowflakes_counter;
+        vector <int> snowflakes;
+        int new_snowflake;
+        while(snowflakes_counter>0)
+        {
+            cin>>new_snowflake;
+            snowflakes.push_back(new_snowflake);
+            snowflakes_counter--;
+        }
+
+        set <int> unique_snowflakes;
+        int first=0;
+        int last=0;
+
+
+        result=0;
+        snowflakes_counter=tmp;
+        while(first < snowflakes_counter)
+        {
+            while(unique_snowflakes.count(snowflakes[last]) == 0)
+            {
+                unique_snowflakes.insert(snowflakes[last]);
+                last++;
+            }
+
+            if(last - first > result)
+            {
+                result = last-first;
+            }
+
+            unique_snowflakes.erase(snowflakes[first]);
+            first++;
+        }
+        /*
+        for(k=snowflakes.size(); k>0; k--)
+        {
+            for(i=0; i<k; i++)
+            {
+                for(j=i; j<k; j++)
+                {
+                    if(snowflakes[i]==snowflakes[j])
+                    {
+                        if(current_max>result)
+                        {result = current_max;}
+                        current_max=0;
+                    }
+                    else
+                    {
+                        current_max++;
+                    }
+                }
+            }
+        }
+        */
+        cout<<result<<endl;
+    cases--;
+    }
+return 0;
+}
+
